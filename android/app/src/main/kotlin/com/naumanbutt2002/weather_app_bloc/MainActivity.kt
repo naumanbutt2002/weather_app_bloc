f1635fb4233dedfdf60d374f57f6b13457a02728
naumanbutt2002 <nauman100percent@gmail.com>
2024-08-17 08:35:54 -0400

    Adding Project Files

---
+++ android/app/src/main/kotlin/com/naumanbutt2002/weather_app_bloc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.naumanbutt2002.weather_app_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/data/my_data.dart
@@ -0,0 +1,1 @@
+String API_KEY = "e9c3df6f212c6b8bb3236724bd119ae5";
